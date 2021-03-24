11f44aa1e73e37159cbfdc3d4d80409e57d9c4b7
Administrator <liguo004>
2021-03-24 19:19:28 +0800

    第二迭代优化

---
+++ app/src/main/java/cn/com/pujing/entity/PublicKey.java
@@ -0,0 +1,27 @@
+package cn.com.pujing.entity;
+
+/**
+ * author : liguo
+ * date : 2021/2/25 16:50
+ * description :
+ */
+public class PublicKey {
+    private String publicKey;
+    private String rsaKey;
+
+    public String getPublicKey() {
+        return publicKey;
+    }
+
+    public void setPublicKey(String publicKey) {
+        this.publicKey = publicKey;
+    }
+
+    public String getRsaKey() {
+        return rsaKey;
+    }
+
+    public void setRsaKey(String rsaKey) {
+        this.rsaKey = rsaKey;
+    }
+}
